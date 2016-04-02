f237cc9e7110e9cc4e733c0b63cf71852a1e3502
William <willpereirabr@gmail.com>
2016-04-02 17:05:06 -0300

    Using buffered access

--- magic.py
+++ magic.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
-with open('airlines.txt','r') as myFile:
-    listStrings = myFile.read().splitlines() 
-    #print(listStrings)
+with open('airlines.txt','r', buffering = 4096) as myFile:
+    listStrings = myFile.read().splitlines() #.splitlines() 'splits' the file into a list 
+    # print(listStrings) show the content of the file in a list
+    # print (myFile)
     count = 0
     for i in listStrings:
         i = i.replace('"','').split(',')
-        # print(i)
+        # print(i) show the content of the file in a list of lists
         if (i[6] == "Brazil"):
         	count += 1
-print("Brazil occurrences on .txt:")
+print("Brazilian airlines on .txt:")
 print(count)        
-myFile.close()	
