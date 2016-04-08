b44aa568578ae72429ac9c6687adc54ea1d28473
William <willpereirabr@gmail.com>
2016-04-07 21:02:08 -0300

    exe2 v0.1

---
+++ uber_magic.py
@@ -0,0 +1,13 @@
+with open('routes.txt','r', buffering = 4096) as f:
+	text = f.read().splitlines()
+	# print (text)
+	countLen = [0,0,0,0,0,0,0,0,0]
+	for i in text:
+		i = i.split(',')
+		# print (i)
+		aux = 0
+		for j in i:
+			# if (len(j) >= countLen[aux])
+			countLen[aux] = len(j)
+			aux += 1
+print (countLen)	
